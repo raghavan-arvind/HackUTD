d9b41498ad876b00421b62d8727ee57b586ede59
Arvind Raghavan <raghavan@wireless-10-148-91-148.public.utexas.edu>
2018-02-25 02:46:23 -0600

    a lot

--- markov_gen.py
+++ markov_gen.py
@@ -4,8 +4,12 @@ import pronouncing as p
 import argparse
 import random
 
+random.seed(1)
+
 parser = argparse.ArgumentParser()
 parser.add_argument('input_file', type=str, help="The name of the input text")
+
+LINE_LENGTH = [6, 9]
 
 class RapIndex:
     def __init__(self):
@@ -13,7 +17,8 @@ class RapIndex:
         self.markovIndex = dict()
 
 
-    def addMarkov(self, key, value):
+    def addMarkov(self, key1, key2, value):
+        key = key1 + "," + key2
         if key in self.markovIndex:
             if value in self.markovIndex[key]:
                 self.markovIndex[key][value] += 1
@@ -24,11 +29,12 @@ class RapIndex:
             entry[value] = 1
             self.markovIndex[key] = entry
     
-    def addRhyme(self, word):
-        if len(word) == 1 and word not in 'ia':
+    def addRhyme(self, word1, word2):
+        word = word1 + "," + word2
+        if len(word1) == 1 and word1 not in 'ia':
             return
 
-        phones = p.phones_for_word(word)
+        phones = p.phones_for_word(word1)
         if len(phones) != 0:
             phones = phones[0].split(" ")
             i = len(phones) - 1
@@ -43,14 +49,25 @@ class RapIndex:
                 stub += phones[i]
                 i -= 1
 
-    def markovNext(self, word):
+    def markovNext(self, word1, word2, no_stop=False, always_stop=False):
+        word = word1+","+word2
+        
         if word not in self.markovIndex:
             raise RuntimeError
+
         choices = []
         for key in self.markovIndex[word]:
             for i in range(self.markovIndex[word][key]):
-                choices.append(key)
-        return random.choice(choices)
+                if no_stop and key == '--':
+                    None # don't add
+                else:
+                    choices.append(key)
+        if always_stop and '--' in choices:
+            return '--'
+        else:
+            if len(choices) == 0:
+                return '--'
+            return random.choice(choices)
 
     def getRhymingWords(self, num=2):
         vowels = [key for key in self.rhymeIndex]
@@ -65,6 +82,8 @@ class RapIndex:
                     wordChoice = random.choice(words)
                     returnList.append(wordChoice)
                     words.remove(wordChoice)
+                if returnList[0].split(',')[0] == returnList[1].split(',')[0]:
+                    return self.getRhymingWords(num=num)
                 return returnList
         return None
     
@@ -73,13 +92,25 @@ class RapIndex:
 
         bars = []
         for word in endWords:
-           current_line = word
-           current_word = word
-           while current_word != '--':
-               current_word = self.markovNext(current_word)
-               if current_word != '--':
-                   current_line = current_word + " " + current_line
-           bars.append(current_line) 
+            current_word1,current_word2 = word.split(",")
+            current_line = current_word2+" "+current_word1
+            num_words = 2
+            while current_word1 != '--':
+                temp = ""
+                if num_words < LINE_LENGTH[0]:
+                    temp = self.markovNext(current_word1,current_word2, no_stop=True)
+                elif num_words > LINE_LENGTH[1]:
+                    temp = self.markovNext(current_word1,current_word2, always_stop=True)
+                else:
+                    temp = self.markovNext(current_word1,current_word2)
+                if temp != '--':
+                    current_line = temp + " " + current_line
+                    current_word1 = current_word2
+                    current_word2 = temp
+                else:
+                    break
+                num_words += 1
+            bars.append(current_line) 
         return bars
 
 
@@ -103,13 +134,22 @@ if __name__ == "__main__":
     print("Building rap index!")
     with open(args.input_file, "r") as f:
         for line in f:
+            line = line.replace("\s+", " ")
             if line.strip() != "":
                 words = line.split(" ")
                 i = len(words) - 1
+                # BUG
                 if i > 0:
-                    index.addRhyme(words[i].strip())
-                while i > 0:
-                    index.addMarkov(words[i].strip(), words[i-1].strip())
+                    index.addRhyme(words[i].strip(), words[i-1].strip())
+                while i > 1:
+                    index.addMarkov(words[i].strip(), words[i-1].strip(), words[i-2].strip())
                     i -= 1
-                index.addMarkov(words[i].strip(), "--")
-    print(index.getBars())
+                index.addMarkov(words[i].strip(), words[i-1].strip(), "--")
+    #print("saving index...")
+    #index.save('index.ind')
+
+
+    print(index.markovIndex)
+    for i in range(2):
+        bars = index.getBars(numBars=2)
+        print(bars[0]+'\n'+bars[1])
