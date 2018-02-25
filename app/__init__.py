25c18c5365eaa7c59cce9598dec2c5eeaa2c0a12
Arvind Raghavan <raghavan@wireless-10-148-91-148.public.utexas.edu>
2018-02-25 04:56:09 -0600

    added instrumental files

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates/", static_folder="templates/", static_url_path="")
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,22 @@
 import sys, os
-from flask import render_template
+from flask import render_template, send_file
+from pydub import AudioSegment
+from gtts import gTTS
 from app import app
 from .markov_gen_one import getLyrics
 
 
 @app.route('/')
 def index():
-    generated = {'lyrics' : getLyrics()}
+    lyr = getLyrics()
+    generated = {'lyrics' : lyr}
+    tts = gTTS(text="    ".join(lyr), lang='en')
+    tts.save("app/templates/rap.mp3")
+    sound1 = AudioSegment.from_mp3("app/templates/rap.mp3")
+    sound2 = AudioSegment.from_mp3("app/templates/ultimate.mp3")
+
+    output = sound2.overlay(sound1,position=20000)
+    output.export("app/templates/mixed_sounds.mp3", format="mp3")
+
     return render_template('index.html', title="Home",generated=generated)
+
